2df2c0d842c8108de0b9eaed2046d58e7085ec17
Zachary Ishler <zri5004@psu.edu>
2023-10-17 13:07:49 -0400

    Remove filtering, undo bucket change (I get it now)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
   const program_code = event.queryStringParameters.program_code;
 
   // Fetch the excel file from the appropriate S3 bucket.
-  const file = await s3.getObject({ Bucket: 'bls-api-preview/' + environment, Key: 'bls-data.xlsx' }).promise();
+  const file = await s3.getObject({ Bucket: 'worldcampus-bls-api/' + environment, Key: 'bls-data.xlsx' }).promise();
 
   // Read all data from the excel file.
   const workbook = xlsx.read(file.Body);
@@ -17,26 +17,26 @@ exports.handler = async (event) => {
   const all_job_titles = xlsx.utils.sheet_to_json(workbook.Sheets['Job Titles']);
 
   // Remove all data except the program we're looking for.
-  const filtered_job_outlooks = all_job_outlooks.filter(row => row.prospect_code === program_code);
-  const filtered_job_titles = all_job_titles.filter(row => row.prospect_code === program_code);
+  // const filtered_job_outlooks = all_job_outlooks.filter(row => row.prospect_code === program_code);
+  // const filtered_job_titles = all_job_titles.filter(row => row.prospect_code === program_code);
 
   // Reduce the job titles data structure to something more usable.
-  const reduced_job_titles = filtered_job_titles.reduce((accumulator, current) => accumulator.concat([current['job_titles']]), []);
+  // const reduced_job_titles = filtered_job_titles.reduce((accumulator, current) => accumulator.concat([current['job_titles']]), []);
 
   // Filter out any unnecessary outlooks information.
-  filtered_job_outlooks.forEach(outlook => {
-    for (const property in outlook) {
-      if (property !== 'occ_title' && property !== 'tot_emp' && property !== 'employment_change') {
-        delete outlook[property];
-      }
-    }
-  });
+  // filtered_job_outlooks.forEach(outlook => {
+  //   for (const property in outlook) {
+  //     if (property !== 'occ_title' && property !== 'tot_emp' && property !== 'employment_change') {
+  //       delete outlook[property];
+  //     }
+  //   }
+  // });
 
   return {
     statusCode: 200,
     body: {
-      job_outlooks: filtered_job_outlooks,
-      job_titles: reduced_job_titles,
+      job_outlooks: all_job_outlooks,
+      job_titles: all_job_titles,
     }
   };
 
