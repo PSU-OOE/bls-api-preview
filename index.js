61f2e2e188c4ecee3c8a6690bce6739a076a76e5
Zachary Ishler <zri5004@psu.edu>
2023-10-18 10:23:05 -0400

    More debugging

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ exports.handler = async function(event, context) {
   return context.logStreamName
 }
 
-exports.handler = async (event) => {
-  const environment = event.queryStringParameters.environment;
-  const program_code = event.queryStringParameters.program_code;
+// exports.handler = async (event) => {
+//   const environment = event.queryStringParameters.environment;
+//   const program_code = event.queryStringParameters.program_code;
 
-  // Fetch the excel file from the appropriate S3 bucket.
-  const file = await s3.getObject({ Bucket: 'worldcampus-bls-api/' + environment, Key: 'bls-data.xlsx' }).promise();
+//   // Fetch the excel file from the appropriate S3 bucket.
+//   const file = await s3.getObject({ Bucket: 'worldcampus-bls-api/' + environment, Key: 'bls-data.xlsx' }).promise();
 
-  // Read all data from the excel file.
-  const workbook = xlsx.read(file.Body);
+//   // Read all data from the excel file.
+//   const workbook = xlsx.read(file.Body);
 
-  // Get the individual sheets for each data source.
-  const all_job_outlooks = xlsx.utils.sheet_to_json(workbook.Sheets['Employment']);
-  const all_job_titles = xlsx.utils.sheet_to_json(workbook.Sheets['Job Titles']);
+//   // Get the individual sheets for each data source.
+//   const all_job_outlooks = xlsx.utils.sheet_to_json(workbook.Sheets['Employment']);
+//   const all_job_titles = xlsx.utils.sheet_to_json(workbook.Sheets['Job Titles']);
 
-  // Remove all data except the program we're looking for.
-  // const filtered_job_outlooks = all_job_outlooks.filter(row => row.prospect_code === program_code);
-  // const filtered_job_titles = all_job_titles.filter(row => row.prospect_code === program_code);
+//   // Remove all data except the program we're looking for.
+//   // const filtered_job_outlooks = all_job_outlooks.filter(row => row.prospect_code === program_code);
+//   // const filtered_job_titles = all_job_titles.filter(row => row.prospect_code === program_code);
 
-  // Reduce the job titles data structure to something more usable.
-  // const reduced_job_titles = filtered_job_titles.reduce((accumulator, current) => accumulator.concat([current['job_titles']]), []);
+//   // Reduce the job titles data structure to something more usable.
+//   // const reduced_job_titles = filtered_job_titles.reduce((accumulator, current) => accumulator.concat([current['job_titles']]), []);
 
-  // Filter out any unnecessary outlooks information.
-  // filtered_job_outlooks.forEach(outlook => {
-  //   for (const property in outlook) {
-  //     if (property !== 'occ_title' && property !== 'tot_emp' && property !== 'employment_change') {
-  //       delete outlook[property];
-  //     }
-  //   }
-  // });
+//   // Filter out any unnecessary outlooks information.
+//   // filtered_job_outlooks.forEach(outlook => {
+//   //   for (const property in outlook) {
+//   //     if (property !== 'occ_title' && property !== 'tot_emp' && property !== 'employment_change') {
+//   //       delete outlook[property];
+//   //     }
+//   //   }
+//   // });
 
-  return {
-    statusCode: 200,
-    body: {
-      job_outlooks: all_job_outlooks,
-      job_titles: all_job_titles,
-    }
-  };
+//   return {
+//     statusCode: 200,
+//     body: {
+//       job_outlooks: all_job_outlooks,
+//       job_titles: all_job_titles,
+//     }
+//   };
 
-};
+// };
